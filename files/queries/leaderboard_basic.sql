0f9f485bac012f8d746b4350cd219f1131fbce2f
Matthew Tebbetts <=>
2024-09-10 21:51:29 -0400

    added leaderboard query

---
+++ files/queries/leaderboard_basic.sql
@@ -0,0 +1,14 @@
+-- alter view matt.leaderboards as
+select
+   	game_name,
+    game_rank,
+    player_name,
+  	game_score,
+    points
+from
+    matt.game_view
+where guild_id = 'global'
+and game_date = curdate()
+order by
+    game_name,
+    coalesce(game_rank, 999);
