f0f0d07b358fe464db7afb20e6cc75a85b9d70b4
Matthew Tebbetts <=>
2025-05-24 18:02:49 -0400

    fixed queries

--- files/queries/active/game_aggregate_stats.sql
+++ files/queries/active/game_aggregate_stats.sql
@@ -9,15 +9,15 @@ WITH game_stats AS (
         COUNT(CASE WHEN game_rank = 3 THEN 1 END) as 3rd,
         COUNT(CASE WHEN game_rank < 6 THEN 1 END) / COUNT(*) as top_5
     FROM games.game_view
-    WHERE game_date BETWEEN %s AND %s
-        AND game_name = %s
+    WHERE game_date BETWEEN '2025-05-01' and '2025-05-24'
+        AND game_name = 'mini'
     GROUP BY player_name
 )
 SELECT 
-    ROW_NUMBER() OVER (ORDER BY points DESC, avg_score ASC) as rank,
+    ROW_NUMBER() OVER (ORDER BY points DESC, avg_score ASC) as `rank`,
     player,
     points,
-    ROUND(avg_score) as avg,
+    ROUND(avg_score) as `avg`,
     wins,
     2nd,
     3rd,
