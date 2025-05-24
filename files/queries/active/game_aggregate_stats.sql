78207a483fae191a8fa99940e550192d994acc9f
Matthew Tebbetts <=>
2025-05-24 18:01:37 -0400

    more

--- files/queries/active/game_aggregate_stats.sql
+++ files/queries/active/game_aggregate_stats.sql
@@ -5,19 +5,24 @@ WITH game_stats AS (
         SUM(points) as points,
         AVG(seconds) as avg_score,
         COUNT(CASE WHEN game_rank = 1 THEN 1 END) as wins,
-        COUNT(CASE WHEN game_rank < 6 THEN 1 END) as top_5
+        COUNT(CASE WHEN game_rank = 2 THEN 1 END) as 2nd,
+        COUNT(CASE WHEN game_rank = 3 THEN 1 END) as 3rd,
+        COUNT(CASE WHEN game_rank < 6 THEN 1 END) / COUNT(*) as top_5
     FROM games.game_view
     WHERE game_date BETWEEN %s AND %s
         AND game_name = %s
     GROUP BY player_name
 )
 SELECT 
+    ROW_NUMBER() OVER (ORDER BY points DESC, avg_score ASC) as rank,
     player,
-    games,
     points,
-    ROUND(avg_score) as avg_score,
+    ROUND(avg_score) as avg,
     wins,
-    ROW_NUMBER() OVER (ORDER BY points DESC, avg_score ASC) as game_rank
+    2nd,
+    3rd,
+    top_5,
+    games
 FROM game_stats
 ORDER BY  points DESC
 ;
