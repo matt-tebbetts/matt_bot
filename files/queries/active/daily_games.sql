1e6d9c0705af4913c98113e245c0838f6753e732
Matthew Tebbetts <=>
2025-06-09 21:34:06 -0400

    updated daily

--- files/queries/active/daily_games.sql
+++ files/queries/active/daily_games.sql
@@ -1,9 +1,9 @@
 select
-	rnk,
-	player,
-	score,
-	detail,
-	bonus,
+	game_rank as rnk,
+	player_name as player,
+	game_score as score,
+	game_detail as detail,
+	game_bonuses as bonus,
 	added_at
 from games.daily_view
 where game_date = %s
